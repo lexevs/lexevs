16158f4e1a12d869de389c54621d10b4a66fb402
hsbauer <bauer.scott@mayo.edu>
2022-11-18 08:44:12 -0600

    Refactoring Test Suite

--- lbTest/src/test/java/org/LexGrid/lexevs/metabrowser/impl/AllTests.java
+++ lbTest/src/test/java/org/LexGrid/lexevs/metabrowser/impl/AllTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
+	MetaBrowserServiceImplTest.class,
 	GetBySourceCountTest.class, 
 	GetBySourceTabDisplayTest.class, 
 	GetMetaTreeTest.class, 
